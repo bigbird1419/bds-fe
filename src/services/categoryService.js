15c98f781de7d9a70f5076041d02dabd5a608931
unknown <truongquoctuan1409@gmail.com>
2024-05-13 15:31:28 +0700

    up category service

---
+++ src/services/categoryService.js
@@ -0,0 +1,31 @@
+import * as httpRequest from '../utils/httpRequest'
+import { CATEGORYS } from '../constants/pathService'
+
+export const getCategory = async (name, code) => {
+    const rs = httpRequest.get(CATEGORYS, { name, code })
+    return rs;
+}
+
+export const getCategoryById = async (id = 1) => {
+    const rs = httpRequest.getById(CATEGORYS, id)
+    return rs;
+}
+
+export const postCategory = async (data = {}) => {
+    const rs = httpRequest.post(CATEGORYS, data)
+    return rs
+}
+
+export const putCategory = async (id, data = {}) => {
+    const rs = httpRequest.post(CATEGORYS, id, data)
+    return rs
+}
+
+export const deleteCategoryById = async (id) => {
+    const rs = httpRequest.deleteById(CATEGORYS, id)
+    return rs
+}
+
+export const deleteCategorys = async (data) => {
+    const rs = httpRequest.deletes(CATEGORYS, data)
+}
